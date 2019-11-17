4f1df6d233829868d7d50617fb17b9f53ba3cedb
alizhan1 <alizhan.akhmetkaliyev@gmail.com>
2019-11-17 15:54:55 +0100

    tests has been added

---
+++ BackEnd/src/test/java/BackApp/RestaurantTest.java
@@ -0,0 +1,23 @@
+package BackApp;
+
+import org.junit.Test;
+
+class RestaurantTest {
+
+    @Test
+    void addToCartTest() {
+    }
+
+    @Test
+    void availableTablesTest() {
+    }
+
+    @Test
+    void getNameTest() {
+    }
+
+    @Test
+    void bookTableTest() {
+    }
+
+}
